4e3f12806c0d73faa955d477e830bfee5647f9eb
John Skilbeck <john.skilbeck@gmail.com>
2016-02-18 11:46:05 -0800

    debugging on server

--- extract.js
+++ extract.js
@@ -9,7 +9,7 @@ var extract = function(db, folder, file, cb){
 };
 
 var loadQuery = function(db, folder, file, cb) {
-	var sqlFile = fs.createReadStream(path.join('./sql'+folder,file+'.sql'));
+	var sqlFile = fs.createReadStream(path.join('./sql/'+folder,file+'.sql'));
 	console.log(sqlFile);
 	sqlFile.on('data',function(chunk){data+=chunk;});
 	sqlFile.on('end',function(){
