f514cce723f83b5a6007eab33156878749b21d7c
John Skilbeck <john.skilbeck@gmail.com>
2016-02-18 11:51:38 -0800

    debugging on server

--- extract.js
+++ extract.js
@@ -10,7 +10,6 @@ var extract = function(db, folder, file, cb){
 
 var loadQuery = function(db, folder, file, cb) {
 	var sqlFile = fs.createReadStream(path.join('./../../sql',folder,file+'.sql'));
-	console.log(sqlFile);
 	sqlFile.on('data',function(chunk){data+=chunk;});
 	sqlFile.on('end',function(){
 		executeQueryPostgres(db, data, cb);		
@@ -21,7 +20,6 @@ var executeQueryPostgres = function(db, sql, cb) {
 	db.connect(function(err){
 		db.query(sql, function(err,result){
 			if (err) console.log(err);
-			console.log(result);
 			cb(result.rows);
 		});	
 	});
