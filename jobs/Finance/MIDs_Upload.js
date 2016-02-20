6b5f81893c8ac171064bdc1ca195b3dd645bfaf1
John Skilbeck <john.skilbeck@gmail.com>
2016-02-19 18:04:50 -0800

    migration!

---
+++ jobs/Finance/MIDs_Upload.js
@@ -0,0 +1,16 @@
+var 
+	path = require('path'),
+	extract = require('./../../extract').extract,
+	transform = require('./../../transform').transform,
+	load = require('./../../load').load,
+	db = 'crostoli', // crostoli or finance
+	html = false, // results as a formatted HTML table?
+	file = path.basename(__filename.replace(/.js$/,'')),
+  folder = __dirname.split(path.sep).pop()
+	; 
+
+extract(db, folder, file, function(data){
+	transform(data, function(data){
+		load(data, folder, file, html);
+	});
+});
---
+++ sql/Finance/Vantiv_Bucket_Analysis.sql
@@ -0,0 +1,23 @@
+declare @now as date, @start as date, @end as date
+
+set @now = getdate()
+set @start = dateadd(mm,(year(@now)- 1900) * 12 + month(@now) - 1 -1 , 0)
+set @end   = dateadd(d,-1 , dateadd(mm,(year(@now)- 1900) * 12 + month(@now)- 1 , 0))  
+
+select 
+       c.Vertical, sum(case when txn.ProcessorId in (6) then txn.Amount else 0 end) as Paymentech, sum(case when txn.ProcessorId in (22) then txn.Amount else 0 end) as Vantiv 
+       -- case when convert(varchar,cast(sum(txn.Amount) as money),1) TPV
+from 
+       YapstoneDM..[Transaction] txn 
+       inner join ETLStaging..FinanceParentTable c on c.PlatformId = txn.PlatformId and c.ChildCompanyId = txn.Ref_CompanyId
+where 
+       c.Vertical in ('Rent', 'Dues','Inn','VRP','SRP','NonProfit')
+       and txn.ProcessorId not in (14,16)
+       and txn.PlatformId in (1,2)
+       and txn.TransactionCycleId in (1) 
+       and txn.ProcessorId in (6,22)
+       and txn.PaymentTypeId in (1,2,3,11,12)
+       and txn.Ref_BatchTypeId in (1,2)
+       and txn.PostDate_R  between @start and @end
+group by
+       c.Vertical
