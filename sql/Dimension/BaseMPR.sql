4c17896e5e5942fb62e8bc91866513a18470d903
John Skilbeck <john.skilbeck@gmail.com>
2016-02-26 17:12:56 -0800

    base mpr change

--- sql/Dimension/BaseMPR.sql
+++ sql/Dimension/BaseMPR.sql
@@ -77,7 +77,7 @@ group by year(txn.PostDate_R), month(txn.PostDate_R),cast(dateadd(d,  0, dateadd
 	case  when pt.Name in ('Visa','Master Card','Discover','Visa Debit','MC Debit') then 'Card' when pt.Name in ('eCheck','Scan') then 'ACH_Scan'  when pt.Name in ('American Express') then case when txn.ProcessorId in (22) and txn.Ref_BatchTypeId in (1) then 'AmEx-Processing' else 'AmEx' end  when pt.name in ('Cash') then 'Cash'     end ,
 	cur.CharCode
 			 
-if object_id('tempdb..#MPR') is not null drop table #MPR   
+if object_id('tempdb..#BaseMPR') is not null drop table #BaseMPR   
 select isnull(txn.Year, billing.Year) Year, isnull(txn.Month, billing.Month) Month, isnull(txn.Date, billing.Date) Date, isnull(txn.PlatformId,billing.PlatformId) PlatformId,isnull(txn.Gateway_Type,'YapProcessing') Gateway,
 	isnull(txn.Vertical,billing.Vertical) Vertical, coalesce(txn.SoftwareName,billing.SoftwareName,'Non-Affiliated')  SoftwareName, isnull(txn.ParentAccountId,billing.ParentAccountId) as ParentAccountId,isnull(txn.ParentName,billing.ParentName) as ParentName ,
 	isnull(txn.Fee_Payment_Type,'PropertyPaid') FeePaymentType ,isnull(txn.Payment_Type,billing.Payment_Type) PaymentTypeGroup ,isnull(txn.Currency,'USD') Currency,
@@ -85,13 +85,14 @@ select isnull(txn.Year, billing.Year) Year, isnull(txn.Month, billing.Month) Mon
 	sum( isnull(txn.Card_Volume_USD,0) ) Card_Volume_USD , sum( isnull(txn.Card_Volume_Net_USD,0)  ) Card_Volume_Net_USD ,
 	sum( isnull(txn.Txn_Count,billing.Txn_Count)) as Txn_Count,
 	sum( isnull( billing.Revenue, 0) ) as Property_Fee,sum( isnull(txn.Property_Fee_USD,0) ) Net_Settled_Fee_USD, sum( isnull(txn.Convenience_Fee_USD,0) ) as Convenience_Fee_USD,sum( isnull( billing.Revenue, 0) + isnull(txn.Property_Fee_USD,0) + isnull(txn.Convenience_Fee_USD,0) ) as Revenue_USD,sum( isnull( billing.Revenue, 0) + isnull(txn.Property_Fee_Net_USD,0) + isnull(txn.Convenience_Fee_Net_USD,0) ) as Revenue_Net_USD, 
+	sum( isnull(txn.Property_Fee_Net_USD,0) ) Net_Settled_Fee_Net_USD,sum( isnull(txn.Convenience_Fee_Net_USD,0) ) as Convenience_Fee_Net_USD,
 	sum( isnull(txn.Credit_Card_USD,0) ) Credit_Card_USD, sum(isnull(txn.Debit_Card_USD, 0) ) Debit_Card_USD, sum(isnull(txn.Amex_Processing_USD,0)) Amex_Processing_USD,
 	sum( isnull(txn.Credit_Card_Net_USD,0) ) Credit_Card_Net_USD, sum(isnull(txn.Debit_Card_Net_USD, 0) ) Debit_Card_Net_USD, sum(isnull(txn.Amex_Processing_Net_USD,0)) Amex_Processing_Net_USD
-	into #MPR
+	into #BaseMPR
 from
 	#txn Txn    
 	full outer join #Billing billing on billing.Year = txn.Year and billing.Month = txn.Month  and billing.PlatformId = txn.PlatformId and billing.ParentAccountId = txn.ParentAccountId and txn.Fee_Payment_Type = 'PropertyPaid'  
-		and txn.Payment_Type = billing.Payment_Type 
+		and txn.Payment_Type = billing.Payment_Type and txn.Gateway_Type = 'YapProcessing'
 group by      
 	isnull(txn.Year, billing.Year) ,isnull(txn.Month, billing.Month) , isnull(txn.Date, billing.Date), isnull(txn.PlatformId,billing.PlatformId), isnull(txn.Gateway_Type,'YapProcessing') ,
 	isnull(txn.Vertical,billing.Vertical) , coalesce(txn.SoftwareName,billing.SoftwareName,'Non-Affiliated') , isnull(txn.ParentAccountId,billing.ParentAccountId) ,isnull(txn.ParentName,billing.ParentName) , 
@@ -105,7 +106,7 @@ select
 	convert(varchar,cast(sum(Card_Volume_USD) as money),1) Card_Volume_USD ,	
 	convert(varchar,cast(sum(Revenue_USD) as money),1) Revenue_USD
 from 
-	#MPR
+	#BaseMPR
 where
 	Gateway in ('YapProcessing')
 group by
