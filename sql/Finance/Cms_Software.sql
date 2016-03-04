30f65bf2751adbdaa6c468f8a27867c9e50c930c
John Skilbeck <john.skilbeck@gmail.com>
2016-03-04 13:18:31 -0800

    Software commissions testing

--- sql/Finance/Cms_Software.sql
+++ sql/Finance/Cms_Software.sql
@@ -1,9 +1,16 @@
 
 declare @now date, @start date, @end date 
 
-set @now = getdate()
+set @now = '2016-02-12'--getdate()
 set @start = dateadd(mm,(year(@now)- 1900) * 12 + month(@now) - 1 -1 , 0)
-set @end   = dateadd(d,-1 , dateadd(mm,(year(@now)- 1900) * 12 + month(@now)- 1 , 0))  
+set @end   = dateadd(d,-1 , dateadd(mm,(year(@now)- 1900) * 12 + month(@now)- 1 , 0)) 
+
+if object_id('tempdb..#Exceptions') is not null drop table #Exceptions 
+create table #Exceptions (ParentName nvarchar(max), SoftwareName nvarchar(max))
+insert into #Exceptions select 'AD Real Estate Investors Inc', 'EZ Payments'
+insert into	#Exceptions select 'Clinton Management', 'EZ Payments'
+insert into	#Exceptions select 'Douglas Elliman Property Management', 'EZ Payments'
+insert into	#Exceptions select 'Fairfield Properties', 'EZ Payments'
 
 if object_id('tempdb..#Softwares') is not null drop table #Softwares
 create table #Softwares (SoftwareName nvarchar(max), CommissionRate decimal(10,2))
@@ -22,22 +29,25 @@ insert into #Softwares select 'Advanced Technology Group  ATG', 0.25
 insert into #Softwares select 'Tenant Technologies' ,  0.25
 insert into #Softwares select 'TotalManagement Properties' , 0.25
 insert into #Softwares select 'TotalManagement',  0.25
+insert into #Softwares select 'EZ Payments',  0.30
 
 	
 select
-	Date , MPR.SoftwareName, ParentName ,
+	Date , coalesce(e.SoftwareName,MPR.SoftwareName) SoftwareName, MPR.ParentName ,
 	sum(TPV_Net_USD) Card_Volume_Net_USD, sum(Revenue_Net_USD) Revenue , sum(COGS_USD) Costs, sum(Revenue_Net_USD) - sum(COGS_USD) Profit, 
 	cast(cast(software.CommissionRate*100 as decimal(10,0)) as varchar)+'%' as Commission_Rate, 
 	software.CommissionRate * (sum(Revenue_Net_USD) - sum(COGS_USD)) Commission 
 from
 	ETLStaging..FinanceMPR MPR
-	join #Softwares software on MPR.SoftwareName = software.SoftwareName
+	left join #Exceptions e on MPR.ParentName = e.ParentName
+	left join #Softwares software on coalesce(e.SoftwareName,MPR.SoftwareName)  = software.SoftwareName
 where
 	MPR.Date in (@end)
 	and MPR.PaymentTypeGroup in ('Card')
 	and Gateway in ('YapProcessing')
+	and MPR.ParentName = 'Fairfield Properties'
 group by
-	Date, MPR.SoftwareName, ParentName, software.CommissionRate
+	Date, coalesce(e.SoftwareName,MPR.SoftwareName), MPR.ParentName, software.CommissionRate
 order by SoftwareName, ParentName
 
 
