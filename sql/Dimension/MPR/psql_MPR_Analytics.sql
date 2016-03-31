a0c102484f2d4806826dbf097b0e723771214652
John Skilbeck <john.skilbeck@gmail.com>
2016-03-31 10:54:11 -0700

    trying to figure out why server runs daily job when testing locally doesn`t

--- sql/Dimension/MPR/psql_MPR_Analytics.sql
+++ sql/Dimension/MPR/psql_MPR_Analytics.sql
@@ -22,46 +22,48 @@ create temp table Allocable_Card_Volume as (
 	group by MPR.Date
 );
 
+create table #COGS_Financials_Base ( Date date, Total_COGS decimal(25,2), Homeaway decimal(25,2), Allocable_Card_Volume decimal(25,2) )														
+
 drop table if exists COGS_Financials_Base;
 create temp table COGS_Financials_Base as (
-	select '2013-01-31' , 4334559 , 765895, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-01-31') )
-	select '2013-02-28' , 3953897 , 731973, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-02-28') )
-	select '2013-03-31' , 3913363 , 721878, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-03-31') )
-	select '2013-04-30' , 3928581 , 777925, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-04-30') )
-	select '2013-05-31' , 5044537 , 1018972, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-05-31') )
-	select '2013-06-30' , 5522725 , 1110487, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-06-30') )
-	select '2013-07-31' , 5457970 , 1196230, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-07-31') )
-	select '2013-08-31' , 3965201 , 873302, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-08-31') )
-	select '2013-09-30' , 3460856 , 860966, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-09-30') )
-	select '2013-10-31' , 3764626 , 882692, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-10-31') )
-	select '2013-11-30' , 4055276 , 974769, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-11-30') )
-	select '2013-12-31' , 4195839 , 1137395, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-12-31') )
-	select '2014-01-31' , 6173394 , 1719081, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-01-31') )
-	select '2014-02-28' , 5255634 , 1596313, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-02-28') )
-	select '2014-03-31' , 5740891 , 1782774, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-03-31') )
-	select '2014-04-30' , 5309589 , 1689962, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-04-30') )
-	select '2014-05-31' , 6702025 , 2131137, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-05-31') )
-	select '2014-06-30' , 7670034 , 2476637, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-06-30') )
-	select '2014-07-31' , 6970573 , 2277938, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-07-31') )
-	select '2014-08-31' , 4975568 , 1698512, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-08-31') )
-	select '2014-09-30' , 5012709 , 1830178, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-09-30') )
-	select '2014-10-31' , 5223066 , 1775187, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-10-31') )
-	select '2014-11-30' , 5080146 , 1920112, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-11-30') )
-	select '2014-12-31' , 6342268 , 2369502, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-12-31') )
-	select '2015-01-31' , 8204391 , 3372823, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-01-31') )
-	select '2015-02-28' , 7263639 , 3017642, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-02-28') )
-	select '2015-03-31' , 8013648 , 3315023, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-03-31') )
-	select '2015-04-30' , 7342937 , 3000951, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-04-30') )
-	select '2015-05-31' , 8717660 , 3503107, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-05-31') )
-	select '2015-06-30' , 10721203 , 4363967, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-06-30') )
-	select '2015-07-31' , 9287115 , 3672875, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-07-31') )
-	select '2015-08-31' , 7186661 , 2889690, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-08-31') )
-	select '2015-09-30' , 6386660 , 2661722, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-09-30') )
-	select '2015-10-31' , 6560096 , 2731206, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-10-31') )
-	select '2015-11-30' , 7485626 , 3187536, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-11-30') )
-	select '2015-12-31' , 7849861 , 3301992, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-12-31') )
-	select '2016-01-31' , 10455477 , 4733567, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2016-01-31') )
-	select '2016-02-29' , 10059903 , 4741161, (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2016-02-29') )
+	select '2013-01-31'::DATE Date , 4334559 Total_COGS , 765895 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-01-31') ) Allocable_Card_Volume union
+	select '2013-02-28'::DATE Date , 3953897 Total_COGS , 731973 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-02-28') ) Allocable_Card_Volume union
+	select '2013-03-31'::DATE Date , 3913363 Total_COGS , 721878 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-03-31') ) Allocable_Card_Volume union
+	select '2013-04-30'::DATE Date , 3928581 Total_COGS , 777925 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-04-30') ) Allocable_Card_Volume union
+	select '2013-05-31'::DATE Date , 5044537 Total_COGS , 1018972 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-05-31') ) Allocable_Card_Volume union
+	select '2013-06-30'::DATE Date , 5522725 Total_COGS , 1110487 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-06-30') ) Allocable_Card_Volume union
+	select '2013-07-31'::DATE Date , 5457970 Total_COGS , 1196230 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-07-31') ) Allocable_Card_Volume union
+	select '2013-08-31'::DATE Date , 3965201 Total_COGS , 873302 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-08-31') ) Allocable_Card_Volume union
+	select '2013-09-30'::DATE Date , 3460856 Total_COGS , 860966 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-09-30') ) Allocable_Card_Volume union
+	select '2013-10-31'::DATE Date , 3764626 Total_COGS , 882692 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-10-31') ) Allocable_Card_Volume union
+	select '2013-11-30'::DATE Date , 4055276 Total_COGS , 974769 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-11-30') ) Allocable_Card_Volume union
+	select '2013-12-31'::DATE Date , 4195839 Total_COGS , 1137395 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2013-12-31') ) Allocable_Card_Volume union
+	select '2014-01-31'::DATE Date , 6173394 Total_COGS , 1719081 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-01-31') ) Allocable_Card_Volume union
+	select '2014-02-28'::DATE Date , 5255634 Total_COGS , 1596313 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-02-28') ) Allocable_Card_Volume union
+	select '2014-03-31'::DATE Date , 5740891 Total_COGS , 1782774 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-03-31') ) Allocable_Card_Volume union
+	select '2014-04-30'::DATE Date , 5309589 Total_COGS , 1689962 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-04-30') ) Allocable_Card_Volume union
+	select '2014-05-31'::DATE Date , 6702025 Total_COGS , 2131137 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-05-31') ) Allocable_Card_Volume union
+	select '2014-06-30'::DATE Date , 7670034 Total_COGS , 2476637 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-06-30') ) Allocable_Card_Volume union
+	select '2014-07-31'::DATE Date , 6970573 Total_COGS , 2277938 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-07-31') ) Allocable_Card_Volume union
+	select '2014-08-31'::DATE Date , 4975568 Total_COGS , 1698512 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-08-31') ) Allocable_Card_Volume union
+	select '2014-09-30'::DATE Date , 5012709 Total_COGS , 1830178 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-09-30') ) Allocable_Card_Volume union
+	select '2014-10-31'::DATE Date , 5223066 Total_COGS , 1775187 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-10-31') ) Allocable_Card_Volume union
+	select '2014-11-30'::DATE Date , 5080146 Total_COGS , 1920112 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-11-30') ) Allocable_Card_Volume union
+	select '2014-12-31'::DATE Date , 6342268 Total_COGS , 2369502 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2014-12-31') ) Allocable_Card_Volume union
+	select '2015-01-31'::DATE Date , 8204391 Total_COGS , 3372823 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-01-31') ) Allocable_Card_Volume union
+	select '2015-02-28'::DATE Date , 7263639 Total_COGS , 3017642 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-02-28') ) Allocable_Card_Volume union
+	select '2015-03-31'::DATE Date , 8013648 Total_COGS , 3315023 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-03-31') ) Allocable_Card_Volume union
+	select '2015-04-30'::DATE Date , 7342937 Total_COGS , 3000951 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-04-30') ) Allocable_Card_Volume union
+	select '2015-05-31'::DATE Date , 8717660 Total_COGS , 3503107 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-05-31') ) Allocable_Card_Volume union
+	select '2015-06-30'::DATE Date , 10721203 Total_COGS , 4363967 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-06-30') ) Allocable_Card_Volume union
+	select '2015-07-31'::DATE Date , 9287115 Total_COGS , 3672875 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-07-31') ) Allocable_Card_Volume union
+	select '2015-08-31'::DATE Date , 7186661 Total_COGS , 2889690 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-08-31') ) Allocable_Card_Volume union
+	select '2015-09-30'::DATE Date , 6386660 Total_COGS , 2661722 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-09-30') ) Allocable_Card_Volume union
+	select '2015-10-31'::DATE Date , 6560096 Total_COGS , 2731206 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-10-31') ) Allocable_Card_Volume union
+	select '2015-11-30'::DATE Date , 7485626 Total_COGS , 3187536 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-11-30') ) Allocable_Card_Volume union
+	select '2015-12-31'::DATE Date , 7849861 Total_COGS , 3301992 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2015-12-31') ) Allocable_Card_Volume union
+	select '2016-01-31'::DATE Date , 10455477 Total_COGS , 4733567 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2016-01-31') ) Allocable_Card_Volume union
+	select '2016-02-29'::DATE Date , 10059903 Total_COGS , 4741161 Homeaway , (select Allocable_Card_Volume from Allocable_Card_Volume where Date in ('2016-02-29') ) Allocable_Card_Volume 
 );
 
 drop table if exists Initial_COGS;
@@ -71,9 +73,9 @@ create temp table Initial_COGS as (
 		sum(
 		case 	when 	MPR.PaymentTypeGroup in ('ACH_Scan','Amex') then (Txn_Count * COGS.ACH) else 0 end +
 		case 	when 	MPR.Vertical not in ('HA') and MPR.PaymentTypeGroup in ('Card','Amex-Processing') then
-								isnull( ((Credit_Card_Net_USD - Amex_Processing_Net_USD) * COGS.Credit * 0.01),0) + isnull((Debit_Card_Net_USD * COGS.Debit * 0.01),0) + isnull((Amex_Processing_Net_USD * COGS.Amex * 0.01),0) + isnull((case when TPV is null and PaymentTypeGroup in ('Card') then TPV_Billing else 0 end * COGS.Blend * 0.01),0)
+								coalesce( ((Credit_Card_Net_USD - Amex_Processing_Net_USD) * COGS.Credit * 0.01),0) + coalesce((Debit_Card_Net_USD * COGS.Debit * 0.01),0) + coalesce((Amex_Processing_Net_USD * COGS.Amex * 0.01),0) + coalesce((case when TPV is null and PaymentTypeGroup in ('Card') then TPV_Billing else 0 end * COGS.Blend * 0.01),0)
 					when 	MPR.Vertical in ('HA') and MPR.PaymentTypeGroup in ('Card','Amex-Processing') and FeePaymentType in ('PropertyPaid') then
-								isnull(COGS_Financials.Homeaway,0)
+								coalesce(COGS_Financials.Homeaway,0)
 								else 0 end
 		) Initial_COGS
 	from
@@ -101,12 +103,12 @@ create temp table MPR as (
 		sum(TPV_USD) TPV_USD,
 		sum(TPV_Net_USD) TPV_Net_USD,
 		sum(Revenue_Net_USD) Revenue_Net_USD ,
-		sum(isnull(
+		sum(coalesce(
 			case 	when MPR.PaymentTypeGroup in ('ACH_Scan','Amex') then (Txn_Count * COGS.ACH) else 0 end +
 			case 	when MPR.Vertical not in ('HA') and MPR.PaymentTypeGroup in ('Card','Amex-Processing') then
-					isnull( ((Credit_Card_Net_USD - Amex_Processing_Net_USD) * COGS.Credit * 0.01),0) + isnull((Debit_Card_Net_USD * COGS.Debit * 0.01),0) + isnull((Amex_Processing_Net_USD * COGS.Amex * 0.01),0) + isnull((case when TPV is null and PaymentTypeGroup in ('Card') then TPV_Billing else 0 end * COGS.Blend * 0.01),0)
-				+(isnull( ( ( cast(Card_Volume_Net_USD as decimal(18,2) ) / cast(COGS_Financials.Allocable_Card_Volume as decimal(18,2)) ) * COGS_Financials.Allocation  ), 0) ) -- Excess
-						when MPR.Vertical in ('HA') and MPR.PaymentTypeGroup in ('Card','Amex-Processing') and MPR.FeePaymentType in ('PropertyPaid') then	isnull(COGS_Financials.Homeaway,0)
+					coalesce( ((Credit_Card_Net_USD - Amex_Processing_Net_USD) * COGS.Credit * 0.01),0) + coalesce((Debit_Card_Net_USD * COGS.Debit * 0.01),0) + coalesce((Amex_Processing_Net_USD * COGS.Amex * 0.01),0) + coalesce((case when TPV is null and PaymentTypeGroup in ('Card') then TPV_Billing else 0 end * COGS.Blend * 0.01),0)
+				+(coalesce( ( ( cast(Card_Volume_Net_USD as decimal(18,2) ) / cast(COGS_Financials.Allocable_Card_Volume as decimal(18,2)) ) * COGS_Financials.Allocation  ), 0) ) -- Excess
+						when MPR.Vertical in ('HA') and MPR.PaymentTypeGroup in ('Card','Amex-Processing') and MPR.FeePaymentType in ('PropertyPaid') then	coalesce(COGS_Financials.Homeaway,0)
 						else 0
 		  end,0)
 		) COGS_USD,
