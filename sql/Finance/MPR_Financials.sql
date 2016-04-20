4d1a6d620aea2c232a2a3b9d2a8500c9eeaaaab5
John Skilbeck <john.skilbeck@gmail.com>
2016-04-20 11:46:04 -0700

    analytics reports

---
+++ sql/Finance/MPR_Financials.sql
@@ -0,0 +1,27 @@
+with dates as (
+	 select	(date_trunc('Month',current_date) - interval '1 day')::date endd 
+)
+select
+	Vertical,
+	sum(TPV_USD)::money as TPV_USD,
+	sum(Revenue)::money as Revenue_Net_USD,
+	sum(COGS_USD)::money as COGS_USD,
+	round((sum(Revenue)-sum(COGS_USD))/sum(Revenue)*100,2) Accounting_Margin
+from
+	MPR
+where date in ( select endd from dates )
+	and vertical not in ('HA-Intl')
+group by
+	Vertical
+union all
+select
+	'Total', 	
+	sum(TPV_USD)::money as TPV_USD,
+	sum(Revenue)::money as Revenue_Net_USD,
+	sum(COGS_USD)::money as COGS_USD,
+	round((sum(Revenue)-sum(COGS_USD))/sum(Revenue)*100,2) Accounting_Margin
+from
+	MPR
+where date in ( select endd from dates )
+	and vertical not in ('HA-Intl')
+;
---
+++ sql/Finance/MPR_Revenue.sql
@@ -0,0 +1,27 @@
+with dates as (
+	 select	(date_trunc('Month',current_date) - interval '1 day')::date endd 
+)
+select
+	Vertical,
+	sum(Convenience_Fee_Net_USD)::money as Convenience_Fee_Net,
+	sum(Net_Settled_Fee_Net_USD)::money as Net_Settled_Fee_Net_USD,	
+	sum(Property_Fee)::money as Monthly_Billing,	
+	sum(Revenue_Net_USD)::money as Total_Revenue
+from
+	mpr_base
+where date in ( select endd from dates )
+	and vertical not in ('HA-Intl')
+group by
+	Vertical
+union all
+select
+	'Total', 	
+	sum(Convenience_Fee_Net_USD)::money as Convenience_Fee_Net,
+	sum(Net_Settled_Fee_Net_USD)::money as Net_Settled_Fee_Net_USD,	
+	sum(Property_Fee)::money as Monthly_Billing,	
+	sum(Revenue_Net_USD)::money as Total
+from
+	mpr_base
+where date in ( select endd from dates )
+	and vertical not in ('HA-Intl')
+;
