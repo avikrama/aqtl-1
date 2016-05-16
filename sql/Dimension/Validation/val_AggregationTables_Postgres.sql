149dfc753b6edbf753e20e5bb4158112d736d22c
John Skilbeck <john.skilbeck@gmail.com>
2016-05-16 14:54:30 -0700

    aggregation tables

--- sql/Dimension/Validation/val_AggregationTables_Postgres.sql
+++ sql/Dimension/Validation/val_AggregationTables_Postgres.sql
@@ -2,7 +2,7 @@ with Date as (
    select (date_trunc('Month',current_date) - interval '1 day')::DATE as StartDate
 			-- select '2016-03-31'::date as StartDate
 ), Analytics as (
-			select /*PlatformId,*/ sum(TPV_USD) TPV_USD, sum(Card_Volume_USD) Card_Volume_USD, 0 Revenue_USD
+			select /*PlatformId,*/ sum(TPV_USD) TPV_USD, sum(Card_Volume_USD) Card_Volume_USD, 0 Revenue_USD, 0 Revenue_Net_USD
 			from  Analytics
 			where Date = ( select StartDate from Date )
 						and Gateway in ('YapProcessing')
@@ -10,7 +10,7 @@ with Date as (
 						and Vertical not in ('Intl')
 			--group by  PlatformId order by 1
 ), TopData as (
-			select /*PlatformId,*/ sum(TPV_USD) TPV_USD, sum(Card_Volume_USD) Card_Volume_USD, sum(Revenue_USD) Revenue_USD
+			select /*PlatformId,*/ sum(TPV_USD) TPV_USD, sum(Card_Volume_USD) Card_Volume_USD, sum(Revenue_USD) Revenue_USD, 0 Revenue_Net_USD
 			from  Top_Data
 			where Date = ( select StartDate from Date )
 						and Gateway in ('YapProcessing')
@@ -18,7 +18,7 @@ with Date as (
 						and Vertical not in ('Intl')
 			--group by  PlatformId order by 1
 ), MPR_Base as (
-			select /*PlatformId,*/ sum(TPV_USD) TPV_USD, sum(Card_Volume_USD) Card_Volume_USD, sum(Revenue_USD) Revenue_USD
+			select /*PlatformId,*/ sum(TPV_USD) TPV_USD, sum(Card_Volume_USD) Card_Volume_USD, sum(Revenue_USD) Revenue_USD, sum(Revenue_Net_USD) Revenue_Net_USD
 			from  MPR_Base
 			where Date = ( select StartDate from Date )
 						and Gateway in ('YapProcessing')
@@ -26,7 +26,7 @@ with Date as (
 						and Vertical not in ('Intl')
 			--group by  PlatformId order by 1
 ), MPR as (
-			select /*PlatformId,*/ sum(TPV_USD) TPV_USD, sum(case when PaymentTypeGroup in ('Card') then TPV_USD else 0 end) Card_Volume_USD, sum(Revenue) Revenue_USD
+			select /*PlatformId,*/ sum(TPV_USD) TPV_USD, sum(case when PaymentTypeGroup in ('Card') then TPV_USD else 0 end) Card_Volume_USD, 0 Revenue_USD, sum(Revenue) Revenue_Net_USD
 			from  MPR
 			where Date = ( select StartDate from Date )
 						and Gateway in ('YapProcessing')
